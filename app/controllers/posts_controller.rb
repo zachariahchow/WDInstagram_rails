5f6d1bef31c763b3cda1913cfacd9202f7d9b708
Zachariah Chow <zachariahchow@gmail.com>
2020-05-09 21:18:39 +0800

    added sort_form partial, updated posts index view/controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,39 @@
+
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if params[:sort_by_date] == 'ascending'
+      @posts = Post.order(:created_at)
+    elsif params[:sort_by_date] == 'descending'
+      @posts = Post.order('created_at DESC')
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+
+    def image_exists?(url)
+      url = URI.parse(url)
+      url.is_a?(URI::HTTP) && !url.host.nil?
+      rescue URI::InvalidURIError
+      false
+      # http = Net::HTTP.start(url.host, url.port)
+
+      # http.head(url.request_uri)['Content-Type'].start_with? 'image'
+    end
+
+    @post = Post.find(params[:id])
+
+    if image_exists?(@post[:photo_url])
+      @image_exists = true
+    end
+
   end
 
   # GET /posts/new
